0684b4ba1b38b0fc20c669c36afd629062649dc2
MohdSahbaz <cshabaz188@gmail.com>
2025-07-01 23:37:18 +0530

    delete seprate banner data

--- server/src/main/java/com/guidespire/repository/home/HomeBannerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.guidespire.repository.home;
-
-import com.guidespire.model.home.HomeBanner;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface HomeBannerRepository extends MongoRepository<HomeBanner, String> {
-}
